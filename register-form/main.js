29d8367fa4cbe01b14b189cbdf358985dbc06792
Karol Ciołczyk <dyweko@gmail.com>
2021-09-09 13:19:29 +0200

    form created, validation added

---
+++ register-form/main.js
@@ -0,0 +1,109 @@
+console.log("sadfdsaf");
+
+const form = document.forms[0];
+const inputElements = document.forms[0].elements;
+const emailInput = document.querySelector(".input-email");
+
+const test = elements.namedItem("house");
+// console.log(test);
+// console.log(form);
+// console.log(inputElements);
+// console.log(emailInput);
+
+let registrationData = {
+  name: "",
+  surname: "",
+  phone: "",
+  street: "",
+  house: "",
+  city: "",
+  email: "",
+};
+let isFormValid = false;
+
+const markEmptyInputs = function (registrationData) {
+  const values = Object.values(registrationData);
+  const objetcEntr = Object.entries(registrationData);
+  const emptyKeys = objetcEntr.filter((array) => array[1] === "");
+  emptyKeys.forEach((array) => {
+    elements.namedItem(`${array[0]}`).classList.add("input--invalid");
+  });
+  const isInputEmpty = values.includes("");
+  if (isInputEmpty) alert("fill empty fields");
+  // if (!isFormValid) alert("fill empty fields");
+
+  console.log(registrationData);
+};
+
+const isInputsCorrect = function (elements) {
+  console.log(
+    Array.from(elements).forEach((element) =>
+      console.log(Array.from(element.classList).includes("input--invalid"))
+    )
+  );
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  isInputsCorrect(inputElements);
+
+  markEmptyInputs(registrationData);
+});
+
+form.addEventListener("input", (event) => {
+  event.preventDefault();
+  const targetName = event.target.name;
+
+  if (targetName === "email") {
+    console.log("email");
+    const isValid = /(\w\.?)+@[\w\.-]+\.\w{2,}/g.test(event.target.value);
+    if (!isValid) event.target.classList.add("input--invalid");
+    if (isValid) event.target.classList.remove("input--invalid");
+  }
+  if (targetName === "phone") {
+    console.log("phone");
+
+    const isValid = /\(?(\d{3})\)?[-\.\s]?(\d{3})[-\.\s]?(\d{3})/g.test(
+      event.target.value
+    );
+    if (!isValid) event.target.classList.add("input--invalid");
+    if (isValid) event.target.classList.remove("input--invalid");
+  }
+  if (
+    targetName === "name" ||
+    targetName === "surname" ||
+    targetName === "city" ||
+    targetName === "street"
+  ) {
+    const isValid = /^[A-Za-z]+$/g.test(event.target.value);
+    if (!isValid) event.target.classList.add("input--invalid");
+    if (isValid) event.target.classList.remove("input--invalid");
+  }
+  if (targetName === "house") {
+    let isValid = /^[0-9]*$/g.test(event.target.value);
+    isValid = event.target.value === "" ? false : isValid;
+    if (!isValid) event.target.classList.add("input--invalid");
+    if (isValid) event.target.classList.remove("input--invalid");
+  }
+  // if (event.target.value === "") {
+  //   event.target.classList.remove("input--invalid");
+  // }
+
+  registrationData = {
+    ...registrationData,
+    [event.target.name]: event.target.value,
+  };
+});
+
+// \(?(\d{3})\)?[-\.\s]?(\d{3})[-\.\s]?(\d{4})
+
+// emailInput.addEventListener("input", (event) => {
+//   // console.log(event.target.value);
+
+//   const isValid = /(\w\.?)+@[\w\.-]+\.\w{2,}/g.test(event.target.value);
+//   console.log(isValid);
+
+//   if (!isValid) event.target.classList.add("input--invalid");
+//   if (isValid) event.target.classList.remove("input--invalid");
+// });
